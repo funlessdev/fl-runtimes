bbd2f71407ea1b4e6a23f32c89858bdcbfa31ae2
Matteo Trentin <matteo.trentin2@unibo.it>
2022-10-02 15:51:53 +0200

    refactor(rust): fix clippy warning in wrapper

--- rust/wrapper/src/main.rs
+++ rust/wrapper/src/main.rs
@@ -26,6 +26,6 @@ fn main() {
         serde_json::from_str(&input).expect("Rust Wrapper: input JSON not well-formatted");
     let output = lib_fl::fl_main(json);
 
-    let _result = serde_json::to_writer(std::io::stdout(), &output)
+    serde_json::to_writer(std::io::stdout(), &output)
         .expect("Rust Wrapper: couldn't format output to json");
 }
