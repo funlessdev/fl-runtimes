b5ed44e3661e157569194545c069650fd16a674e
giusdp <dpl.gsp@tutanota.com>
2022-10-15 10:49:57 +0200

    build: add musl-dev to dockerfile and small refactors

--- rust/init_script.sh
+++ rust/init_script.sh
@@ -13,7 +13,8 @@
 # limitations under the License.
 
 #!/bin/sh
+mkdir /proj/lib_fl
 cp -r /lib_fl/* /proj/lib_fl/
 echo "$(cat /proj/lib_fl/Cargo.toml | dasel put string -r toml 'package.name' 'lib_fl')" > /proj/lib_fl/Cargo.toml
 cargo wasi build --release
-cp /proj/target/wasm32-wasi/release/wrapper.wasm /out_wasm/code.wasm
+mv /proj/target/wasm32-wasi/release/wrapper.wasm /out_wasm/code.wasm
